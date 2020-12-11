3999450a456ccf1d1f9167f347c9e028a5c25c6f
Ken Obata <kenichiderumo@gmail.com>
2020-12-11 14:58:49 -0800

    12/11: created go routine and ticker() in main.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"Scraping/app/controllers"
 	_ "Scraping/app/controllers"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -15,17 +14,6 @@ import (
 
 func main() {
 	log.Println("os.Getenv:", os.Getenv("MONGO_SERVER"))
-	// 1. で定義したMongoDBクライアント作成関数から構造体を取得
-	mongoClient, err := controllers.ConnectMongoDB() //mongoClient is a pointer of address to DB.
-	fmt.Println("my mongoClient:", mongoClient)
-	if err != nil {
-		fmt.Println("Error from ConnectMongoDB()!")
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	var url [2]string
-	url[0] = "https://www.linkedin.com/jobs/search/?geoId=101174742&keywords=intern&location=Canada"
-	url[1] = "https://www.glassdoor.ca/Job/canada-software-engineer-internship-jobs-SRCH_IL.0,6_IN3_KO7,35.htm"
 
 	server := http.Server{} //if you use kubectl
 	if os.Getenv("MONGO_SERVER") == "" {
@@ -40,24 +28,46 @@ func main() {
 	//add css below
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css")))) //http.Handle("/css/")
 
-	mongoClient.DoMongoImport()
+	go ticker()
+	server.ListenAndServe()
+}
+
+func ticker() {
+	var url [2]string
+	url[0] = "https://www.linkedin.com/jobs/search/?geoId=101174742&keywords=intern&location=Canada"
+	url[1] = "https://www.glassdoor.ca/Job/canada-software-engineer-internship-jobs-SRCH_IL.0,6_IN3_KO7,35.htm"
+
+	// 1. で定義したMongoDBクライアント作成関数から構造体を取得
+	mongoClient, err := controllers.ConnectMongoDB() //mongoClient is a pointer of address to DB.
+	if err != nil {
+		log.Println("Error from ConnectMongoDB()!", err)
+		os.Exit(1)
+	}
+
+	if os.Getenv("MONGO_SERVER") != "" {
+		mongoClient.DoMongoImport()
+	}
+
 	for i := range url {
 		mongoClient.GetURL(url[i])
 	}
-	server.ListenAndServe()
 
-	t := time.NewTicker(12 * time.Hour)
+	t := time.NewTicker(7 * time.Second)
 	for {
 		select {
 		case <-t.C:
-			// every t hour, run get URL function.
-			mongoClient.DoMongoImport()
-			//2. web crawl　and store into mongo
-			for i := range url {
-				mongoClient.GetURL(url[i])
-			}
-		}
-	}
+			log.Println("ticker is working.")
+			/*
+				if os.Getenv("MONGO_SERVER") != "" {
+					// this is in kubernetes cluster
+					mongoClient.DoMongoImport()
+				} else { //this is in localhost
+					//web crawl　and store into mongo
+					for i := range url {
+						mongoClient.GetURL(url[i])
+					}
+				}*/
+		} //end of one t transaction.
+	} //end of for loop
 	t.Stop()
-
 }
