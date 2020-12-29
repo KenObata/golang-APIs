0b8d82e66dfe687eb5a9436d1770b08fad3ee648
Ken Obata <kenichiderumo@gmail.com>
2020-12-28 18:19:37 -0800

    12/28: createdatabase.sql created.

--- flyway/init/create_database.sql
+++ flyway/init/create_database.sql
@@ -1,1 +1,18 @@
+DROP DATABASE IF EXISTS test_db;
 CREATE DATABASE test_db;
+
+\c test_db;
+
+CREATE TABLE IF NOT EXISTS job(id serial PRIMARY KEY, company varchar(255) NOT NULL, title varchar(255) NOT NULL, url varchar(255) NOT NULL, dateadded varchar(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS user_list(id serial PRIMARY KEY, email varchar(255) NOT NULL, password varchar(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS user_job(id serial PRIMARY KEY, user_id int, job_id int);
+
+ALTER TABLE user_job
+ADD CONSTRAINT user_id_fk
+FOREIGN KEY (user_id) REFERENCES user_list (id);
+
+ALTER TABLE user_job
+ADD CONSTRAINT job_id_fk
+FOREIGN KEY (job_id) REFERENCES job (id);
+
+INSERT INTO user_list(email,password) SELECT 'kenichiderumo@gmail.com', '2030';
