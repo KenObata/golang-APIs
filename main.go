e3c4ac6d29e0db2b8a298ffab8ea9a2ea1c2c62a
Ken Obata <kenichiderumo@gmail.com>
2020-12-28 22:05:40 -0800

    12/28: main.go replaces mongo to postgres.

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css")))) //http.Handle("/css/")
 
 	controllers.Init_db() //initialize Postgres
-	controllers.Init()    //initialize Redis
+	//controllers.Init()    //initialize Redis
 
 	go ticker()
 
@@ -47,14 +47,16 @@ func ticker() {
 	url[0] = "https://www.linkedin.com/jobs/search/?geoId=101174742&keywords=intern&location=Canada"
 	url[1] = "https://www.glassdoor.ca/Job/canada-software-engineer-internship-jobs-SRCH_IL.0,6_IN3_KO7,35.htm"
 
-	mongoClient, err := controllers.ConnectMongoDB() //mongoClient is a pointer of address to DB.
+	/*mongoClient, err := controllers.ConnectMongoDB() //mongoClient is a pointer of address to DB.
 	if err != nil {
 		log.Println("Error from ConnectMongoDB()!", err)
 		os.Exit(1)
 	}
+	*/
 
 	for i := range url {
-		err := mongoClient.GetURL(url[i])
+		//err := mongoClient.GetURL(url[i])
+		err := controllers.GetURL2(url[i])
 		if err != nil {
 			log.Println("error from mongoClient.GetURL(): ", err)
 		}
@@ -62,7 +64,8 @@ func ticker() {
 
 	//clean up DB once
 	log.Println("Run DeleteDuplicate().")
-	deleteError := mongoClient.DeleteDuplicate()
+	//deleteError := mongoClient.DeleteDuplicate()
+	deleteError := controllers.DeleteJobDuplicate()
 	if deleteError != nil {
 		log.Println("erro from DeleteDuplicate(): ", deleteError)
 	}
@@ -76,11 +79,16 @@ func ticker() {
 			if os.Getenv("MONGO_SERVER") == "" { //this is in localhost
 				//web crawl　and store into mongo
 				for i := range url {
-					mongoClient.GetURL(url[i])
+					//mongoClient.GetURL(url[i])
+					err := controllers.GetURL2(url[i])
+					if err != nil {
+						log.Println("error from mongoClient.GetURL(): ", err)
+					}
 				}
 			} else { //on kubernetes cluster
 				//clean up DB
-				deleteError := mongoClient.DeleteDuplicate()
+				//deleteError := mongoClient.DeleteDuplicate()
+				deleteError = controllers.DeleteJobDuplicate()
 				if deleteError != nil {
 					log.Println("error from DeleteDuplicate()")
 					log.Println(deleteError)
