a50a709a8d961b0c2b9d0e2b86de35e3a3d35423
Ken Obata <kenichiderumo@gmail.com>
2020-12-28 16:19:44 -0800

    12/28: postgres init, sql files created. postgres.go created. db service added to docker-compose.yml

---
+++ flyway/sql/V0.001__create_table.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS job(id int, company varchar(255), title varchar(255), url varchar(255), dateadded company varchar(255));
+CREATE TABLE IF NOT EXISTS user(id int, email varchar(255), password varchar(255));
